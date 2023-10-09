82f36853ec16ea6304a4491d2f4809bd18bc9d73
Patrikas <patrikas.armalis@gmail.com>
2023-10-09 12:48:54 +0300

    Create Poison.cs

---
+++ _Eligijus/Scripts/Abilities/Poison.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace Rivencrestgodot._Eligijus.Scripts.Abilities;
+
+public class Poison
+{
+    public Node2D Poisoner;
+    public ChunkData chunk;
+    public int turnsLeft;
+    public int poisonValue;
+    
+    public Poison(ChunkData chunk, int turnsleft, int poisonvalue)
+    {
+        this.chunk = chunk;
+        turnsLeft = turnsleft;
+        poisonValue = poisonvalue;
+    }
+}
