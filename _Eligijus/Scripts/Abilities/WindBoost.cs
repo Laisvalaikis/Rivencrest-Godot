ea8f6f41c9ea01b314ea89b41c21cf2656930518
Eligijus <e.kiudys@gmail.com>
2023-10-10 16:08:57 +0300

    Windboost

---
+++ _Eligijus/Scripts/Abilities/WindBoost.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+public partial class WindBoost : BaseAction
+{
+    private bool isAbilityActive = false;
+    
+    public override void OnTurnStart()//pradzioj ejimo
+    {
+        base.OnTurnStart();
+        isAbilityActive = false;
+    }
+    public override void ResolveAbility(ChunkData chunk)
+    {
+        base.ResolveAbility(chunk);
+        isAbilityActive = true;
+        FinishAbility();
+    }
+
+}
