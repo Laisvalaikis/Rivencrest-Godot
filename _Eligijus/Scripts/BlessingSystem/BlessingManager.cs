0786f7485d1d90ba18fd095887cd033340330d2e
Eligijus <e.kiudys@gmail.com>
2023-10-12 17:17:50 +0300

    Blessing manager

--- _Eligijus/Scripts/BlessingSystem/BlessingManager.cs
+++ _Eligijus/Scripts/BlessingSystem/BlessingManager.cs
@@ -1,19 +1,28 @@
+using System;
 using Godot;
 using Godot.Collections;
 using Array = System.Array;
 
 public partial class BlessingManager : Node
 {
+    
     private Array<PlayerBlessing> _playerBlessings;
     private Array<AbilityBlessing> _abilityBlessings;
+    
+    private Array<PlayerBlessing> _playerBlessingsTemp;
+    private Array<AbilityBlessing> _abilityBlessingsTemp;
+    
     private Data _data;
-
+    private Array<BaseBlessing> _generatedBlessings;
+    private Random _random;
     public override void _Ready()
     {
         base._Ready();
         if (Data.Instance != null)
         {
             _data = Data.Instance;
+            SetupBlessingLists();
+            _random = new Random();
         }
     }
 
@@ -67,5 +76,46 @@ public partial class BlessingManager : Node
                 _playerBlessings.Add(playerBlessings[i]);
             }
         }
+    }
+
+    private void GenerateBlessingList()
+    {
+        _playerBlessingsTemp = new Array<PlayerBlessing>(_playerBlessings);
+        _abilityBlessingsTemp = new Array<AbilityBlessing>(_abilityBlessings);
+        if (_generatedBlessings == null)
+        {
+            _generatedBlessings = new Array<BaseBlessing>();
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+
+            int maxRange = _abilityBlessingsTemp.Count + _playerBlessingsTemp.Count - 1;
+            int index = _random.Next(0, maxRange);
+
+            if (index < _playerBlessingsTemp.Count)
+            {
+                if (!_generatedBlessings.Contains(_playerBlessingsTemp[index]))
+                {
+                    _generatedBlessings.Add(_playerBlessingsTemp[index]);
+                    _playerBlessingsTemp.RemoveAt(index);
+                }
+            }
+            else if (index >= _playerBlessingsTemp.Count)
+            {
+                int correctIndex = _playerBlessings.Count - index;
+                if (!_generatedBlessings.Contains(_abilityBlessingsTemp[correctIndex]))
+                {
+                    _generatedBlessings.Add(_abilityBlessingsTemp[correctIndex]);
+                    _abilityBlessingsTemp.RemoveAt(correctIndex);
+                }
+            }
+        }
+
+    }
+
+    private Array<BaseBlessing> GeneratedBlessings()
+    {
+        return _generatedBlessings;
     }
 }
