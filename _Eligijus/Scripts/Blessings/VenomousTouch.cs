eaa2ddfa16145eabb3970ee31cf6f7cfc91a84ba
EligijusK <e.kiudys@gmail.com>
2023-10-16 21:24:14 +0300

    No errors

--- _Eligijus/Scripts/Blessings/VenomousTouch.cs
+++ _Eligijus/Scripts/Blessings/VenomousTouch.cs
@@ -1,38 +1,38 @@
 using Godot;
 
-public class VenomousTouch : AbilityBlessing
+public partial class VenomousTouch : AbilityBlessing
 {
-    [Export] private int poisonCounut = 1;
-    [Export] private int poisonTurns = 2;
-    [Export] private int poisonDamage = 2;
-    
-    public VenomousTouch()
-    {
+	[Export] private int poisonCounut = 1;
+	[Export] private int poisonTurns = 2;
+	[Export] private int poisonDamage = 2;
+	
+	public VenomousTouch()
+	{
 			
-    }
-    
-    public VenomousTouch(VenomousTouch blessing): base(blessing)
-    {
-        poisonCounut = blessing.poisonCounut;
-        poisonTurns = blessing.poisonTurns;
-        poisonDamage = blessing.poisonDamage;
-    }
-    
-    public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
-    {
-        VenomousTouch blessing = new VenomousTouch((VenomousTouch)baseBlessing);
-        return blessing;
-    }
-    
-    public override BaseBlessing CreateNewInstance()
-    {
-        VenomousTouch blessing = new VenomousTouch(this);
-        return blessing;
-    }
+	}
+	
+	public VenomousTouch(VenomousTouch blessing): base(blessing)
+	{
+		poisonCounut = blessing.poisonCounut;
+		poisonTurns = blessing.poisonTurns;
+		poisonDamage = blessing.poisonDamage;
+	}
+	
+	public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
+	{
+		VenomousTouch blessing = new VenomousTouch((VenomousTouch)baseBlessing);
+		return blessing;
+	}
+	
+	public override BaseBlessing CreateNewInstance()
+	{
+		VenomousTouch blessing = new VenomousTouch(this);
+		return blessing;
+	}
 
-    public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
-    {
-        base.ResolveBlessing(ref baseAction);
-        baseAction.AddPoison(new Poison(tile, 2, 2));
-    }
+	public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
+	{
+		base.ResolveBlessing(ref baseAction);
+		baseAction.AddPoison(new Poison(tile, 2, 2));
+	}
 }
