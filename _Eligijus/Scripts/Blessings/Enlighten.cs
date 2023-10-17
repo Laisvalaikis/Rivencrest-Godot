fa6c49262f83a1c32e64e427099940aeb90f5436
Eligijus <e.kiudys@gmail.com>
2023-10-17 14:06:01 +0300

    Blessings

---
+++ _Eligijus/Scripts/Blessings/Enlighten.cs
@@ -0,0 +1,45 @@
+using System;
+using Godot;
+
+public partial class Enlighten : AbilityBlessing
+{
+
+    [Export] private int minHeal = 3;
+    [Export] private int maxHeal = 5;
+    private Random _random;
+    
+    public Enlighten()
+    {
+			
+    }
+    
+    public Enlighten(Enlighten blessing): base(blessing)
+    {
+        _random = new Random();
+        minHeal = blessing.minHeal;
+        maxHeal = blessing.maxHeal;
+    }
+    
+    public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
+    {
+        Enlighten blessing = new Enlighten((Enlighten)baseBlessing);
+        return blessing;
+    }
+    
+    public override BaseBlessing CreateNewInstance()
+    {
+        Enlighten blessing = new Enlighten(this);
+        return blessing;
+    }
+    
+    public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
+    {
+        base.ResolveBlessing(ref baseAction, tile);
+        if (tile.CharacterIsOnTile())
+        {
+            int heal = _random.Next(minHeal, maxHeal);
+            tile.GetCurrentPlayerInformation().Heal(heal);
+        }
+    }
+    
+}
