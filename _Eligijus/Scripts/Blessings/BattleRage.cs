7b525cd1a5e1110ea263d6aead4d3fda3b7c3b0b
Eligijus <e.kiudys@gmail.com>
2023-10-18 11:09:27 +0300

    Abilities

---
+++ _Eligijus/Scripts/Blessings/BattleRage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Godot;
+
+public partial class BattleRage : AbilityBlessing
+{
+    [Export] private int minHeal = 3;
+    [Export] private int maxHeal = 4;
+    private Random _random;
+    private bool secondTime = false;
+    
+    public BattleRage()
+    {
+			
+    }
+    
+    public BattleRage(BattleRage blessing): base(blessing)
+    {
+        _random = new Random();
+        minHeal = blessing.minHeal;
+        maxHeal = blessing.maxHeal;
+    }
+    
+    public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
+    {
+        BattleRage blessing = new BattleRage((BattleRage)baseBlessing);
+        return blessing;
+    }
+    
+    public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
+    {
+        base.ResolveBlessing(ref baseAction);
+       
+        if (!secondTime)
+        {
+            secondTime = true;
+        }
+        else
+        {
+            int randomHeal = _random.Next(minHeal, maxHeal);
+            baseAction.GetPlayer().playerInformation.Heal(randomHeal);
+            secondTime = false;
+        }
+    }
+}
---
+++ _Eligijus/Scripts/Blessings/ExplosivePoison.cs
@@ -0,0 +1,57 @@
+﻿using Godot;
+using System.Collections.Generic;
+public partial class ExplosivePoison : AbilityBlessing
+{
+    [Export] private int poisonTurns = 2;
+    [Export] private int poisonDamage = 2;
+    
+    public ExplosivePoison()
+    {
+			
+    }
+    
+    public ExplosivePoison(ExplosivePoison blessing): base(blessing)
+    {
+        poisonTurns = blessing.poisonTurns;
+        poisonDamage = blessing.poisonDamage;
+    }
+    
+    public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
+    {
+        ExplosivePoison blessing = new ExplosivePoison((ExplosivePoison)baseBlessing);
+        return blessing;
+    }
+    
+    public override BaseBlessing CreateNewInstance()
+    {
+        ExplosivePoison blessing = new ExplosivePoison(this);
+        return blessing;
+    }
+    
+    public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
+    {
+        base.ResolveBlessing(ref baseAction);
+        (int x, int y) = tile.GetIndexes();
+        
+        var directionVectors = new List<(int, int)>
+        {
+            (1 + x, 0 + y),
+            (0 + x, 1 + y),
+            (-1 + x, 0 + y),
+            (0 + x, -1 + y)
+        };
+
+        foreach (var directions in directionVectors)
+        {
+            ChunkData chunkData = GameTileMap.Tilemap.GetChunkDataByIndex(directions.Item1, directions.Item2);
+            if (chunkData.CharacterIsOnTile())
+            {
+                PlayerInformation player = chunkData.GetCurrentPlayerInformation();
+                if (IsAllegianceSame(player, chunkData, baseAction))
+                {
+                    baseAction.AddPoison(new Poison(chunkData, poisonTurns, poisonDamage));
+                }
+            }
+        }
+    }
+}
