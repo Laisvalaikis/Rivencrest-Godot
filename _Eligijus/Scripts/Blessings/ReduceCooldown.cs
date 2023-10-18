56243048f361e310d5d3d5e0070a911d85983fd6
Eligijus <e.kiudys@gmail.com>
2023-10-18 15:05:57 +0300

    Reduce Cooldown

--- _Eligijus/Scripts/Blessings/ReduceCooldown.cs
+++ _Eligijus/Scripts/Blessings/ReduceCooldown.cs
@@ -2,5 +2,42 @@
 
 public partial class ReduceCooldown : AbilityBlessing
 {
+    [Export] private int _reduceCooldown = 2;
     
+    public ReduceCooldown()
+    {
+			
+    }
+    
+    public ReduceCooldown(ReduceCooldown blessing): base(blessing)
+    {
+        _reduceCooldown = blessing._reduceCooldown;
+    }
+    
+    public override BaseBlessing CreateNewInstance(BaseBlessing baseBlessing)
+    {
+        ReduceCooldown blessing = new ReduceCooldown((ReduceCooldown)baseBlessing);
+        return blessing;
+    }
+    
+    public override BaseBlessing CreateNewInstance()
+    {
+        ReduceCooldown blessing = new ReduceCooldown(this);
+        return blessing;
+    }
+
+    public override void ResolveBlessing(ref BaseAction baseAction, ChunkData tile)
+    {
+        base.ResolveBlessing(ref baseAction, tile);
+        if (tile.GetCurrentPlayerInformation() != null)
+        {
+            PlayerInformation player = tile.GetCurrentPlayerInformation();
+            if (IsAllegianceSame(baseAction.GetPlayer().playerInformation, tile, baseAction))
+            {
+                // Need to redo abilityPoints on player
+                GD.PrintErr("Redo CooldownPoints on player");
+                baseAction.abilityCooldown -= _reduceCooldown;
+            }
+        }
+    }
 }
