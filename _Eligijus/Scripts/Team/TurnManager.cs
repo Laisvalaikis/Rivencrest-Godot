7bf67d28370c8f5a7f8626110f57e327def377b6
Eligijus <e.kiudys@gmail.com>
2023-10-30 15:20:00 +0200

    Turn manager in progress

--- _Eligijus/Scripts/Team/TurnManager.cs
+++ _Eligijus/Scripts/Team/TurnManager.cs
@@ -2,13 +2,35 @@
 
 public class TurnManager : Node
 {
-    [Export] private Team currentTeam;
-    [Export] private int currentTeamIndex = 0;
-    [Export] private Player currentPlayer;
-    
+    [Export] private Team _currentTeam;
+    [Export] private int _currentTeamIndex = 0;
+    [Export] private TeamsList _teamsList;
+    [Export] private Player _currentPlayer;
+    private Vector2 _mousePosition;
+    public void StartTurn()
+    {
+        OnTurnStart();
+    }
+
+    public void EndTurn()
+    {
+        OnTurnEnd();
+        if (_currentTeamIndex + 1 < _teamsList.Teams.Count)
+        {
+            _currentTeamIndex++;
+        }
+        else
+        {
+            _currentTeamIndex = 0;
+        }
+        _currentTeam = _teamsList.Teams[_currentTeamIndex];
+        OnTurnStart();
+        
+    }
+
     public void OnTurnStart()
     {
-        foreach (Player character in currentTeam.characters)
+        foreach (Player character in _currentTeam.characters)
         {
             character.OnTurnStart();
         }
@@ -16,18 +38,29 @@ public class TurnManager : Node
 
     public void OnTurnEnd()
     {
-        foreach (Player character in currentTeam.characters)
+        foreach (Player character in _currentTeam.characters)
         {
             character.OnTurnEnd();
         }
     }
+    
+    public void OnMove(Vector2 position)
+    {
+        _mousePosition = position;
+        // ChunkData hoveredChunk = GameTileMap.Tilemap.GetChunk(_mousePosition);
+        // _currentAbility.Action.OnMoveArrows(hoveredChunk,_previousChunk);
+        // _currentAbility.Action.OnMoveHover(hoveredChunk,_previousChunk);
+        // _previousChunk = hoveredChunk;
+    }
 
     public void ResolveAbility()
     {
-        foreach (Player character in currentTeam.characters)
-        {
-            // character.actionManager.Ex;
-        }
+        ChunkData chunk = GameTileMap.Tilemap.GetChunk(_mousePosition);
+
+        // foreach (Player character in _currentTeam.characters)
+        // {
+        //     // character.actionManager.Ex;
+        // }
     }
 
 }
