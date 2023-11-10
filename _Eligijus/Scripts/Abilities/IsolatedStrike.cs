d3f55774de07b1b4a81fe1448d35ca12d13b4757
Patrikas <patrikas.armalis@gmail.com>
2023-11-10 17:29:44 +0200

    ability

--- _Eligijus/Scripts/Abilities/IsolatedStrike.cs
+++ _Eligijus/Scripts/Abilities/IsolatedStrike.cs
@@ -5,6 +5,8 @@ public partial class IsolatedStrike : BaseAction
     [Export]
     private int isolationDamage = 7;
 
+    private int bonusDamage = 0;
+
     public IsolatedStrike()
     {
         
@@ -17,6 +19,65 @@ public partial class IsolatedStrike : BaseAction
     {
         IsolatedStrike ability = new IsolatedStrike((IsolatedStrike)action);
         return ability;
+    }
+    
+    public override void EnableDamagePreview(ChunkData chunk, string text = null)
+    {
+        HighlightTile highlightTile = chunk.GetTileHighlight();
+        if (customText != null)
+        {
+            highlightTile.SetDamageText(customText);
+        }
+        else
+        {
+            if (maxAttackDamage == minAttackDamage)
+            {
+                highlightTile.SetDamageText((maxAttackDamage+bonusDamage).ToString());
+            }
+            else
+            {
+                highlightTile.SetDamageText($"{minAttackDamage+bonusDamage}-{maxAttackDamage+bonusDamage}");
+            }
+
+            if (chunk.GetCurrentPlayer()!=null && chunk.GetCurrentPlayer().playerInformation.GetHealth() <= minAttackDamage)
+            {
+                highlightTile.ActivateDeathSkull(true);
+            }
+        }
+    }
+    
+    public override void OnMoveHover(ChunkData hoveredChunk, ChunkData previousChunk)
+    {
+        HighlightTile previousChunkHighlight = previousChunk?.GetTileHighlight();
+        HighlightTile hoveredChunkHighlight = hoveredChunk?.GetTileHighlight();
+
+        if (previousChunkHighlight != null && (hoveredChunk == null || !hoveredChunkHighlight.isHighlighted))
+        {
+            SetNonHoveredAttackColor(previousChunk);
+            DisableDamagePreview(previousChunk);
+        }
+        if (hoveredChunkHighlight == null || hoveredChunk == previousChunk)
+        {
+            return;
+        }
+        if (hoveredChunkHighlight.isHighlighted)
+        {
+            SetHoveredAttackColor(hoveredChunk);
+            if (CanTileBeClicked(hoveredChunk))
+            {
+                if (IsTargetIsolated(hoveredChunk))
+                {
+                    bonusDamage += isolationDamage;
+                }
+                EnableDamagePreview(hoveredChunk);
+                bonusDamage = 0;
+            }
+        }
+        if (previousChunkHighlight != null)
+        {
+            SetNonHoveredAttackColor(previousChunk);
+            DisableDamagePreview(previousChunk);
+        }
     }
     
     public override void ResolveAbility(ChunkData chunk)
@@ -48,11 +109,10 @@ public partial class IsolatedStrike : BaseAction
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (GameTileMap.Tilemap.CheckBounds(nx, ny) && chunks[nx, ny].CharacterIsOnTile() && IsAllegianceSame(chunks[nx, ny]))
+            if (GameTileMap.Tilemap.CheckBounds(nx, ny) && chunks[nx, ny].CharacterIsOnTile() && chunks[nx, ny].GetCurrentPlayer().GetPlayerTeam()==target.GetCurrentPlayer().GetPlayerTeam())
             {
                 return false;
             }
-            
         }
         return true;
     }
