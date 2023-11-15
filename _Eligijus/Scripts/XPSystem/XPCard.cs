7419cae50e8b5b9d5744e51e63f5e169436eb3e2
Eligijus <e.kiudys@gmail.com>
2023-11-15 15:46:06 +0200

    Xp manager in testing

--- _Eligijus/Scripts/XPSystem/XPCard.cs
+++ _Eligijus/Scripts/XPSystem/XPCard.cs
@@ -8,9 +8,12 @@ public partial class XPCard : Control
     [Export] private Label xp;
     [Export] private Label levelText;
     [Export] private Label xpToGain;
+    [Export] private int updateTextInMs = 10;
     private int _tempXPToGain;
+    private int _tempCurrentXp;
     private SavedCharacterResource _characterResource;
     private Data _data;
+    private bool updatingXp = false;
 
     public void UpdateXPCard(SavedCharacterResource savedCharacterResource)
     {
@@ -28,13 +31,15 @@ public partial class XPCard : Control
         int currentXP = _data.XPToLevelUp[_characterResource.level] -
                         (_data.XPToLevelUp[_characterResource.level] - _characterResource.xP);
         xp.Text = currentXP + "/" + _data.XPToLevelUp[_characterResource.level] + " XP";
-        DelayMethod();
+        UpdateXPGradualy();
     }
 
-    private async Task DelayMethod()
+    private async Task UpdateXPGradualy()
     {
-        int tempXp = _characterResource.xP + _tempXPToGain;
-        while (_characterResource.xP < tempXp)
+        _tempCurrentXp = _characterResource.xP;
+        int tempXp = _tempCurrentXp + _tempXPToGain;
+        updatingXp = true;
+        while (_characterResource.xP < tempXp && updatingXp)
         {
             if (_data.XPToLevelUp[_characterResource.level] > _characterResource.xP)
             {
@@ -42,21 +47,82 @@ public partial class XPCard : Control
                 int currentXP = _data.XPToLevelUp[_characterResource.level] -
                                 (_data.XPToLevelUp[_characterResource.level] - _characterResource.xP);
                 xp.Text = currentXP + "/" + _data.XPToLevelUp[_characterResource.level] + " XP";
+                if (_data.XPToLevelUp[_characterResource.level] <= _characterResource.xP)
+                {
+                    if (_characterResource.level < _data.XPToLevelUp.Count)
+                    {
+                        _characterResource.xP -= _data.XPToLevelUp[_characterResource.level];
+                        tempXp -= _data.XPToLevelUp[_characterResource.level];
+                        _characterResource.level++;
+                        _characterResource.abilityPointCount++;
+                        levelText.Text = _characterResource.level.ToString();
+                        if (_characterResource.level >= _data.XPToLevelUp.Count)
+                        {
+                            xp.Text = "Max Level";
+                            break;
+                        }
+                        else
+                        {
+                            
+                            xp.Text = "0/" + _data.XPToLevelUp[_characterResource.level] + " XP";
+                        }
+                    }
+                    else
+                    {
+                        levelText.Text = _characterResource.level.ToString();
+                        xp.Text = "Max Level";
+                        break;
+                    }
+                }
             }
-            else
+
+            await Task.Delay(TimeSpan.FromMilliseconds(updateTextInMs)); // wait for 500ms 
+        }
+        _characterResource.xPToGain = 0;
+    }
+
+    public void UpdateXP()
+    {
+        if (updatingXp)
+        {
+            updatingXp = false;
+        }
+        int tempXp = _tempCurrentXp + _tempXPToGain;
+        while (_characterResource.xP < tempXp)
+        {
+            int leftTillNextLevel = _data.XPToLevelUp[_characterResource.level] - _characterResource.xP;
+            if (leftTillNextLevel <= tempXp)
             {
                 if (_characterResource.level < _data.XPToLevelUp.Count)
                 {
+                    _characterResource.xP = 0;
                     _characterResource.level++;
                     _characterResource.abilityPointCount++;
                     levelText.Text = _characterResource.level.ToString();
+                    if (_characterResource.level < _data.XPToLevelUp.Count)
+                    {
+                        xp.Text = tempXp + "/" + _data.XPToLevelUp[_characterResource.level] + " XP"; 
+                    }
+                    else
+                    {
+                        levelText.Text = _characterResource.level.ToString();
+                        xp.Text = "Max Level";
+                        break;
+                    }
+                    tempXp -= leftTillNextLevel;
                 }
                 else
                 {
+                    levelText.Text = _characterResource.level.ToString();
+                    xp.Text = "Max Level";
                     break;
                 }
             }
-            await Task.Delay(TimeSpan.FromMilliseconds(100)); // wait for 500ms 
+            else
+            {
+                _characterResource.xP = tempXp;
+                xp.Text = tempXp + "/" + _data.XPToLevelUp[_characterResource.level] + " XP";
+            }
         }
         _characterResource.xPToGain = 0;
     }
