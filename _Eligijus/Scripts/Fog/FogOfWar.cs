8a67ba711922c9456ab78e72344653dbcf22a661
Eligijus <e.kiudys@gmail.com>
2023-11-22 10:06:03 +0200

    Fog of war script works

--- _Eligijus/Scripts/Fog/FogOfWar.cs
+++ _Eligijus/Scripts/Fog/FogOfWar.cs
@@ -3,37 +3,33 @@
 public partial class FogOfWar : Sprite2D
 {
     [Export] private Image lightImage;
-    [Export] private int GridSize = 100;
-    [Export] private int width = 10;
-    [Export] private int height = 10;
-    private Image fogImage = new Image();
-    private ImageTexture fogTexture = new ImageTexture();
+    [Export] private int GridSize = 10;
+    [Export] private int width = 2000;
+    [Export] private int height = 2000;
+    private Image fogImage;
+    private ImageTexture fogTexture;
     private Vector2I lightOffset;
     public override void _Ready()
     {
         base._Ready();
-        // lightImage.Resize(3, 3, Image.Interpolation.Nearest);
-        lightOffset = new Vector2I(lightImage.GetWidth() / 2, lightImage.GetHeight() / 2);
-        int fogImageWidth = 1000 / GridSize;
-        var fogImageHeight = 1000 / GridSize;
-        fogImage = Image.Create(fogImageWidth, fogImageHeight, false, Image.Format.Rgbah);
+        lightImage.Resize(100, 100, Image.Interpolation.Nearest);
+        lightOffset = new Vector2I((width / 2) - (lightImage.GetWidth() / 2), (width / 2) - (lightImage.GetHeight() / 2));
+        fogImage = Image.Create(width, height, false, Image.Format.Rgbah);
         fogImage.Fill(Colors.Black);
         lightImage.Convert(Image.Format.Rgbah);
         fogTexture = ImageTexture.CreateFromImage(fogImage);
         Texture = fogTexture;
-        Scale *= GridSize;
     }
 
     public void UpdateFog(Vector2I gridPosition)
     {
         Rect2I lightRectI = new Rect2I(Vector2I.Zero, new Vector2I(lightImage.GetWidth(), lightImage.GetHeight()));
-        fogImage.BlendRect(lightImage, lightRectI, gridPosition);
+        fogImage.BlendRect(lightImage, lightRectI, gridPosition + lightOffset);
         UpdateForImageTexture();
     }
 
     public void UpdateForImageTexture()
     {
-        // fogTexture.Create
         fogTexture = ImageTexture.CreateFromImage(fogImage);
         Texture = fogTexture;
     }
@@ -41,7 +37,8 @@ public partial class FogOfWar : Sprite2D
     public override void _Input(InputEvent @event)
     {
         Vector2 position = GetGlobalMousePosition();
-        Vector2I vector2I = new Vector2I(Mathf.FloorToInt(position.X), Mathf.FloorToInt(position.Y)) ;
-        UpdateFog(vector2I/GridSize);
+        position = ToLocal(position);
+        Vector2I vector2I = new Vector2I(Mathf.RoundToInt(position.X), Mathf.FloorToInt(position.Y)) ;
+        UpdateFog(vector2I);
     }
 }
