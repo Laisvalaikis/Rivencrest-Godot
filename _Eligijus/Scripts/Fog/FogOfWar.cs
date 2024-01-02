bf60b3aab61ad98a6223c85a25487b62b11bd339
Eligijus <e.kiudys@gmail.com>
2024-01-02 16:59:44 +0200

    Fog

--- _Eligijus/Scripts/Fog/FogOfWar.cs
+++ _Eligijus/Scripts/Fog/FogOfWar.cs
@@ -19,13 +19,17 @@ public partial class FogOfWar : Sprite2D
     public override void _Ready()
     {
         base._Ready();
-        lightImage.Resize(Mathf.CeilToInt((float)lightImage.GetWidth()/ (float)scale), Mathf.CeilToInt((float)lightImage.GetHeight()/(float)scale), Image.Interpolation.Nearest);
+        // lightImage.Resize(Mathf.CeilToInt((float)lightImage.GetWidth()/ (float)scale), Mathf.CeilToInt((float)lightImage.GetHeight()/(float)scale), Image.Interpolation.Nearest);
+        lightImage = Image.Create(Mathf.CeilToInt(100.0 / scale), Mathf.CeilToInt(100.0 / scale), false, Image.Format.Rgbah);
         lightOffset = new Vector2I((width / 2) - (lightImage.GetWidth() / 2), (height / 2) - (lightImage.GetHeight() / 2));
         addFog = Image.Create(lightImage.GetWidth(), lightImage.GetHeight(), false, Image.Format.Rgbah);
         fogRectI = new Rect2I(Vector2I.Zero, new Vector2I(addFog.GetWidth(), addFog.GetHeight()));
         fogOffset = new Vector2I((width / 2) - (addFog.GetWidth() / 2), (height / 2) - (addFog.GetHeight() / 2));
         addFog.Fill(Colors.Black);
-        lightImage.Convert(Image.Format.Rgbah);
+        Color color = Colors.Black;
+        color.A = 0;
+        lightImage.Fill(color);
+        // lightImage.Convert(Image.Format.Rgbah);
         Scale *= scale;
         _shaderMaterial = (Material as ShaderMaterial);
     }
@@ -39,6 +43,7 @@ public partial class FogOfWar : Sprite2D
     
     public void SetFogTexture(ImageTexture fog)
     {
+        // oneTime = true;
         UpdateForImageTexture(fog);
     }
     
@@ -55,21 +60,23 @@ public partial class FogOfWar : Sprite2D
     
     public void RemoveFog(Vector2 position, Team characterTeam)
     {
-        Image image = characterTeam.fogTexture.GetImage();
+       
         Vector2 gridPosition = ToLocal(position);
         Vector2I gridPositionI = new Vector2I(Mathf.RoundToInt(gridPosition.X), Mathf.FloorToInt(gridPosition.Y));
-        if (!oneTime)
-        {
+        // if (!oneTime)
+        // {
             Vector2 offsetImage = new Vector2(lightImage.GetWidth() / 2, lightImage.GetHeight() / 2);
             Vector2 realPosition = gridPositionI + lightOffset + offsetImage;
             Vector2 vec = new Vector2(realPosition.X / (float)width, realPosition.Y / (float)height);
             _shaderMaterial.SetShaderParameter("fog_position", vec);
-            oneTime = true;
-        }
-        
+            // oneTime = true;
+        // }
+        Image image = characterTeam.fogTexture.GetImage();
+        // characterTeam.fogTexture.
+        // Image img = Image.CreateFromData(width, height, false, Image.Format.Rgbah, image.GetData());
         // image.BlendRect(lightImage, fogRectI, gridPositionI + lightOffset);
         image.BlitRect(lightImage, fogRectI, gridPositionI + lightOffset);
-        characterTeam.fogTexture.Update(image);
+        // characterTeam.fogTexture.Update(img);
         // UpdateForImageTexture();
     }
 
