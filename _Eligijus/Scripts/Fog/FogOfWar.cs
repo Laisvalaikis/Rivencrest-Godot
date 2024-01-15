a5933c8c2420a61d300b481c2170101cd82148f3
Eligijus <e.kiudys@gmail.com>
2024-01-15 16:22:07 +0200

    Fixing Scale shit

--- _Eligijus/Scripts/Fog/FogOfWar.cs
+++ _Eligijus/Scripts/Fog/FogOfWar.cs
@@ -4,36 +4,29 @@ using Godot;
 
 public partial class FogOfWar : Sprite2D
 {
-    [Export] private Image lightImage;
-    [Export] private int GridSize = 10;
     [Export] private int scale = 3;
     [Export] private int width = 3000;
     [Export] private int height = 3000;
-    private Image fogImage;
-    private Image addFog;
-    private ImageTexture fogTexture;
-    private Vector2I lightOffset;
-    private Vector2I fogOffset;
-    private Rect2I fogRectI;
+    private Vector2 lightImage;
+    private Vector2 lightOffset;
     private bool oneTime = false;
     private ShaderMaterial _shaderMaterial;
     public override void _Ready()
     {
         base._Ready();
         // lightImage.Resize(Mathf.CeilToInt((float)lightImage.GetWidth()/ (float)scale), Mathf.CeilToInt((float)lightImage.GetHeight()/(float)scale), Image.Interpolation.Nearest);
-        lightImage = Image.Create(Mathf.CeilToInt(100.0 / scale), Mathf.CeilToInt(100.0 / scale), false, Image.Format.Rgbah);
-        lightOffset = new Vector2I((width / 2) - (lightImage.GetWidth() / 2), (height / 2) - (lightImage.GetHeight() / 2));
-        addFog = Image.Create(lightImage.GetWidth(), lightImage.GetHeight(), false, Image.Format.Rgbah);
-        fogRectI = new Rect2I(Vector2I.Zero, new Vector2I(addFog.GetWidth(), addFog.GetHeight()));
-        fogOffset = new Vector2I((width / 2) - (addFog.GetWidth() / 2), (height / 2) - (addFog.GetHeight() / 2));
-        addFog.Fill(Colors.Black);
+        lightImage =  new Vector2((float)100.0 / scale, (float)100.0 / scale);
+        lightOffset = new Vector2((width / 2) - (lightImage.X / 2), (height / 2) - (lightImage.Y / 2));
         Color color = Colors.Black;
         color.A = 0;
-        lightImage.Fill(color);
-        // lightImage.Convert(Image.Format.Rgbah);
+        // Scale = Vector2.One;
         Scale *= scale;
+        // lightImage.Convert(Image.Format.Rgbah);
         _shaderMaterial = (Material as ShaderMaterial);
         UpdateForImageTexture();
+        // Scale *= scale;
+        
+        
     }
     
     public void AddFog(Vector2 position, Team characterTeam)
@@ -50,32 +43,29 @@ public partial class FogOfWar : Sprite2D
     public void RemoveFog(Vector2 maxPosition, Vector2[] visionTiles, int lenght)
     {
         
-        Vector2 squareSize = lightImage.GetSize();
-        squareSize = new Vector2(squareSize.X / (float)width, squareSize.Y / (float)height) - new Vector2((float)0.00066625, (float)0.000666625);
+        Vector2 squareSize = lightImage;
+        squareSize = new Vector2(squareSize.X / (float)width, squareSize.Y / (float)height); // - new Vector2((float)0.0006661, (float)0.0006661);
         
         _shaderMaterial.SetShaderParameter("square_size", squareSize);
         _shaderMaterial.SetShaderParameter("fog_max_position", maxPosition);
         _shaderMaterial.SetShaderParameter("fog_position_array_size", lenght);
         _shaderMaterial.SetShaderParameter("fog_position_array", visionTiles);
-       
+        
     }
     
     public void UpdateCharacterPositions(Vector2[] characterPositions, int characterPositionsCount)
     {
         _shaderMaterial.SetShaderParameter("fog_player_position_array", characterPositions);
         _shaderMaterial.SetShaderParameter("fog_player_position_array_size", characterPositionsCount);
-        if (characterPositionsCount > 2)
-        {
-            GD.Print(characterPositions[2]);
-        }
     }
 
     public Vector2 GenerateFogPosition(Vector2 position)
     {
         Vector2 gridPosition = ToLocal(position);
-        Vector2 offsetImage = new Vector2(lightImage.GetWidth() / 2, lightImage.GetHeight() / 2);
+        Vector2 offsetImage = new Vector2(lightImage.X / 2, lightImage.Y / 2);
         Vector2 realPosition = gridPosition + lightOffset + offsetImage;
-        Vector2 vec = new Vector2(realPosition.X / (float)width, realPosition.Y / (float)height);
+        Vector2 vec = new Vector2(realPosition.X / (float)width, realPosition.Y / (float)height); // smth is off with position calculation
+        GD.Print(vec);
         return vec;
     }
 
@@ -87,7 +77,7 @@ public partial class FogOfWar : Sprite2D
     
     private void UpdateForImageTexture()
     {
-        fogImage = Image.Create(width, height, false, Image.Format.Rgbah);
+        Image fogImage = Image.Create(width, height, false, Image.Format.Rgbah);
         fogImage.Fill(Colors.Black);
         Texture =  ImageTexture.CreateFromImage(fogImage);
     }
